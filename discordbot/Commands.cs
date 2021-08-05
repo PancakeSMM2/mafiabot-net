a784d09fc3bf8aadd4cb7eca49d99e866c958d33
PancakeSMM2 <smm2pancake@gmail.com>
2021-08-04 19:55:28 -0500

    Add project files.

---
+++ discordbot/Commands.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace discordbot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        /** 
+         The Ping command.
+         In future, I desire to add a millisecond delay display
+         */
+        [Command("ping")]
+        [Summary("Replies with \"Pong!\".")]
+        public async Task PingAsync()
+        {
+            long receivedAt = Context.Message.Timestamp.ToUnixTimeMilliseconds();
+            await ReplyAsync("Pong!").ContinueWith((Task<IUserMessage> task) =>
+            {
+                long delay = task.Result.Timestamp.ToUnixTimeMilliseconds() - receivedAt;
+                task.Result.ModifyAsync((MessageProperties properties) =>
+                {
+                    properties.Content = $"Pong! {delay}s delay";
+                });
+            });
+
+        }
+
+        [Command("changestatus")]
+        [Summary("Changes the bot's status.")]
+        [Alias("status", "statuschange", "activity", "changeactivity", "setactivity")]
+        [Priority(1)]
+        public async Task ChangeStatusAsync([Summary("The status type to set. One of the following: PLAYING STREAMING LISTENING WATCHING COMPETING")] string statusType, [Remainder] [Summary("The status text to set.")] string newStatus)
+        {
+            IEmote[] emotes = new IEmote[2];
+
+            switch(statusType.ToUpper()) {
+                case "PLAYING":
+                    await Context.Client.SetActivityAsync(new Game(newStatus, ActivityType.Playing));
+                    emotes[0] = new Emoji("✅");
+                    emotes[1] = new Emoji("🎮");
+                    break;
+                case "STREAMING":
+                    await Context.Client.SetActivityAsync(new Game(newStatus, ActivityType.Streaming));
+                    emotes[0] = new Emoji("✅");
+                    emotes[1] = new Emoji("🎮");
+                    break;
+                case "LISTENING":
+                    await Context.Client.SetActivityAsync(new Game(newStatus, ActivityType.Listening));
+                    emotes[0] = new Emoji("✅");
+                    emotes[1] = new Emoji("🎮");
+                    break;
+                case "WATCHING":
+                    await Context.Client.SetActivityAsync(new Game(newStatus, ActivityType.Watching));
+                    emotes[0] = new Emoji("✅");
+                    emotes[1] = new Emoji("🎮");
+                    break;
+                default:
+                    await Context.Client.SetActivityAsync(new Game(statusType + " " + newStatus));
+                    emotes[0] = new Emoji("✅");
+                    emotes[1] = new Emoji("🎮");
+                    break;
+            }
+
+            await Context.Message.AddReactionsAsync(emotes);
+        }
+
+        [Command("changestatus")]
+        [Summary("Changes the bot's status.")]
+        [Alias("status", "statuschange", "activity", "changeactivity", "setactivity")]
+        [Priority(0)]
+        public async Task ChangeStatusAsync([Remainder][Summary("The status text to set.")] string newStatus)
+        {
+            await Context.Client.SetActivityAsync(new Game(newStatus));
+            IEmote[] emotes = new IEmote[2];
+            emotes[0] = new Emoji("✅");
+            emotes[1] = new Emoji("🎮");
+            await Context.Message.AddReactionsAsync(emotes);
+        }
+
+    }
+}
---
+++ discordbot/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace discordbot
+{
+    class Program
+    {
+        public static void Main()
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            CommandHandler handler = new CommandHandler(_client, _commands);
+
+            await handler.InstallCommandsAsync();
+
+            _client.Log += Log;
+
+            // TEMPORARY SOLUTION
+            string token = "NzE1NTk0MTIxMTg3ODE5NjEw.Xs_e9Q.41eA_Ptt4X5Vqy8ucKcB6Dun72E";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            
+            // Ensures this task will never complete
+            await Task.Delay(-1);
+        }
+
+        static public Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        // Retrieve client and CommandService via constructor
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            // Hook the MessageReceived event into the command handler
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
+                                            services: null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a system message
+            if (!(messageParam is SocketUserMessage message)) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+
+            // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+                return;
+
+            // Create a WebSocket-based command context based on the message
+            SocketCommandContext context = new SocketCommandContext(_client, message);
+
+            // Execute the command with the command context, along with the service provider for precondition checks
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
+        }
+    }
+}
