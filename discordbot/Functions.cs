b6d7e59703e4b157574812a470c7b8aaea6c2126
PancakeSMM2 <smm2pancake@gmail.com>
2021-08-10 22:02:12 -0500

    Add different emoji reactions to ChangeStatusAsync. Add ImagesOnlyAsync. Create class Bouncer in Program.cs

---
+++ discordbot/Functions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace mafiabot
+{
+    static class Functions
+    {
+        public static async Task<bool> ToggleSnowflakeFromJSONAsync(ulong snowflake, string filePath)
+        {
+            return await Task.Run(() =>
+            {
+                string content = Convert.ToBase64String(File.ReadAllBytes(filePath));
+                ulong[] output = JsonConvert.DeserializeObject<ulong[]>(content);
+
+                bool remove = output.Contains(snowflake);
+                if (remove) {
+                    int index = Array.IndexOf(output, snowflake);
+                    Array.Clear(output, index, 1);
+                } else output.Append(snowflake);
+
+                string newContent = JsonConvert.SerializeObject(output);
+                File.WriteAllBytes(filePath, Convert.FromBase64String(newContent));
+                return remove;
+            });
+        }
+    }
+}
